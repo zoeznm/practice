fb73e8e722c6125b64cd9bad7b13bbccdcecd91d
zoeznm <bomikim046@gmail.com>
2024-06-11 00:31:07 +0900

    이미지 슬라이드 구현 완료

--- js/a-2.js
+++ js/a-2.js
@@ -1,27 +1,12 @@
-window.onload = function() {
-  var submenu = document.querySelector(".submenu");
-  var mainmenu = document.querySelector(".mainmenu");
-
-  submenu.style.height = '0';
-  submenu.style.overflow = 'hidden';
-  submenu.style.transition = 'height 0.4s ease';
-
-  mainmenu.onmouseover = function() {
-    submenu.style.height = submenu.scrollHeight + 'px';
-  };
-
-  mainmenu.onmouseout = function() {
-    submenu.style.height = '0';
-  };
-};
-setInterval(function(){
-  $('#slide1>ul').delay(3000);
-  $('#slide1>ul').animate({marginLeft: "-1200px"})
-  $('#slide1>ul').delay(3000);
-  $('#slide1>ul').animate({marginLeft: "-2400px"})
-  $('#slide1>ul').delay(3000);
-  $('#slide1>ul').animate({marginLeft: "0px"})
+$(document).ready(function(){
+  setInterval(function(){
+      $('.img_box').animate({left: '-1200px'}, 1000, function(){
+          $(this).css({left: 0}).append($(this).find('img:first'));
+      });
+  }, 3000);
 });
+
+
 
 
 
