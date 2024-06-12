53ecd5481efed6339b64d861f538553ddec456cc
zoeznm <bomikim046@gmail.com>
2024-06-12 21:39:01 +0900

    f-1

---
+++ js/f-1.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+// ! 메인메뉴
+$('.navi > li').mouseover(function(){
+  $(this).find('.subMenu').stop().slideDown(300);
+}).mouseout(function(){
+  $(this).find('.subMenu').stop().slideUp(300);
+});
+
+// ! 이미지 슬라이드
+
+// ! 탭메뉴
+
+// ! 모달창
+});
