de94ef4d5f054fb53c3f2b9f33d6c93b7affb6aa
zoeznm <bomikim046@gmail.com>
2024-06-12 10:27:38 +0900

    팝업 전까지 완료

--- js/a-1.js
+++ js/a-1.js
@@ -7,14 +7,19 @@ $(".mainMenu").mouseover(function(){
   $(".subMenu").stop().slideUp(300);
 });
 // ! 슬라이드
-// $(".slide > a:gt(0)").hide();
+$(".slide > a:gt(0)").hide();
 
-// setInterval(function(){
-//   $('.slide a:first-child').fadeOut()
-//   .next('a').fadeIn()
-//   .end().appendTo('.slide');
-// }, 3000);
+setInterval(function(){
+  $('.slide a:first-child').fadeOut()
+  .next('a').fadeIn()
+  .end().appendTo('.slide');
+}, 3000);
 // ! 탭메뉴
-
+$(function(){
+  $(".tabMenu > li > a").click(function(){
+    $(this).parent().addClass("active").siblings().removeClass("active");
+     return false;
+  });
+});
 // ! 모달창 
 });
