a03bc019e42816b13bba221204bf4541968e12f0
zoeznm <bomikim046@gmail.com>
2024-06-13 17:07:55 +0900

    c-3

--- js/c-3.js
+++ js/c-3.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 // ! mainmenu
+$('.navi > li').mouseover(function(){
+  $(this).find('.subMenu').stop().slideDown(300);
+}).mouseout(function(){
+  $(this).find('.subMenu').stop().slideUp(300);
+});
 
 // ! slide
 
