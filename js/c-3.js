c2fb5ca38947f16e358858753f4be7eb7c5a1be1
zoeznm <bomikim046@gmail.com>
2024-06-13 21:47:13 +0900

    수정

--- js/c-3.js
+++ js/c-3.js
@@ -7,6 +7,20 @@ $('.navi > li').mouseover(function(){
 });
 
 // ! slide
+var imgs = 2;
+var now = 0;
 
+start();
+
+function start(){
+  $(".imgSlide>a").eq(0).siblings().animate({width:"-800px"});
+
+  setInterval(function(){
+    now = now==imgs ? 0 : now+=1;
+    $(".imgSlide>a").eq(now-1).animate({width:"-800px"});
+    $(".imgSlide>a").eq(now).animate({width:"800px"});
+  },3000);
+}
 // ! modal
+
 });
